e83c87ee27a4b28fb1f5111f3959bafb40cf8c4e
Ignacio07 <65515081+Ignacio07@users.noreply.github.com>
2023-12-14 23:56:49 -0300

    Update middle.service.ts

--- src/middle/middle.service.ts
+++ src/middle/middle.service.ts
@@ -5,6 +5,7 @@ https://docs.nestjs.com/providers#services
 import { Injectable } from '@nestjs/common';
 import { MemberService } from 'src/members/member.service';
 import { TeamService } from 'src/teams/team.service';
+import { CreateMemberTeamDto } from './dto/create.member.team.dto';
 
 
 @Injectable()
@@ -27,6 +28,17 @@ export class MiddleService {
         console.log(teamsInfo);
         return teamsInfo;
         
+    }
+
+    async addMemberToTeam({ email, id }: CreateMemberTeamDto) {
+        const id_team = parseInt(id,10);
+        console.log(id_team);
+        const existingTeam = await this.teamService.findOne(id_team);
+        if (!existingTeam) {
+            throw new Error('El Equipo no existe');
+        }
+        const add = await this.memberService.create({email, id_team})
+        return add;
     }
 
     /*async changeTeamName(data: { email: string , id}): Promise<string> {
